b41840aaf838708364ce4f18bfac987bfbbe1ca5
SebSa12000 <sebastien.salgues@gmail.com>
2025-10-06 13:08:06 +0200

    create coach in bdd via script

--- create_users.sql
+++ create_users.sql
@@ -1,6 +1,17 @@
 insert into User( id,first_name,last_name,email,password,is_admin,created_at,updated_at,is_coach,is_nutrition,is_subscribe,adress1,adress2,postal_code,city,allergy_comment,physical_constraint,coach_certif,coach_experience,coach_description,size,weight, picture )
 values ( '83726b63-dcd3-4f7e-bc36-0f326ae5972f','Sebastien','Salgues','sebastien.salgues@gmail.com','$2b$12$9XQ4sIB26KdtyY7Aiq8jjeMqFuJ9fRgFWiaiBpi7xlvPklUZ5.GHG',1,'2025-09-15 17:05:36.374418','2025-09-15 17:05:36.374432',0,0,0,'23 Avenue Hippolyte de Barrau','','12000','Rodez','Allergique aux pollens','Pas trop dur !','','','',0.0,0.0,'')
 
+insert into User( id,first_name,last_name,email,password,is_admin,created_at,updated_at,is_coach,is_nutrition,is_subscribe,adress1,adress2,postal_code,city,allergy_comment,physical_constraint,coach_certif,coach_experience,coach_description,size,weight, picture )
+values ( '83726b63-dcd3-4f7e-bc36-0f326ae59721','Sebastien','Salgues','sebastien.salgues@hotmail.com','$2b$12$9XQ4sIB26KdtyY7Aiq8jjeMqFuJ9fRgFWiaiBpi7xlvPklUZ5.GHG',1,'2025-09-15 17:05:36.374418','2025-09-15 17:05:36.374432',1,1,0,'23 Avenue Hippolyte de Barrau','','12000','Rodez','','','',"8 ans d'experience",'Expert en musculation',0.0,0.0,'http://localhost:5500/public/images/ready/coach2.jpg')
+
+insert into User( id,first_name,last_name,email,password,is_admin,created_at,updated_at,is_coach,is_nutrition,is_subscribe,adress1,adress2,postal_code,city,allergy_comment,physical_constraint,coach_certif,coach_experience,coach_description,size,weight, picture )
+values ( '83726b63-dcd3-4f7e-bc36-0f326ae59722','Elhadj','Reziga','seba15@laposte.net','$2b$12$9XQ4sIB26KdtyY7Aiq8jjeMqFuJ9fRgFWiaiBpi7xlvPklUZ5.GHG',1,'2025-09-15 17:05:36.374418','2025-09-15 17:05:36.374432',1,1,0,'23 Avenue Hippolyte de Barrau','','12000','Rodez','','','',"5 ans d'experience",'Spécialiste Fitness & Nutrition',0.0,0.0,'http://localhost:5500/public/images/ready/coach1.jpg')
+
+
+insert into User( id,first_name,last_name,email,password,is_admin,created_at,updated_at,is_coach,is_nutrition,is_subscribe,adress1,adress2,postal_code,city,allergy_comment,physical_constraint,coach_certif,coach_experience,coach_description,size,weight, picture )
+values ( '83726b63-dcd3-4f7e-bc36-0f326ae59723','Sarah','Reziga','sebastien.salgues@hotmail.com','$2b$12$9XQ4sIB26KdtyY7Aiq8jjeMqFuJ9fRgFWiaiBpi7xlvPklUZ5.GHG',1,'2025-09-15 17:05:36.374418','2025-09-15 17:05:36.374432',1,1,0,'23 Avenue Hippolyte de Barrau','','12000','Rodez','','','',"20 ans d'experience",'Expert en musculation',0.0,0.0,'http://localhost:5500/public/images/ready/coach3.jpg')
+
+
 insert into "Product_shop" ("id", "name", "description", 
     "picture", "picture2", "picture3",
     "price", "created_at", "updated_at",
