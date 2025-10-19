e6559298e9afafe5d8480a255e8e2a41524aa1df
hedjouj <messaoui.elhadj@hotmail.fr>
2025-10-19 14:15:22 +0200

    fix: corrected path - class rename - rename variables - and fix logic in variables

--- app/api/v1/subscription.py
+++ app/api/v1/subscription.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Namespace('subscription', description='Subscription operations')
 
-# Define the review model for input validation and documentation
+# Define the subscription model for input validation and documentation
 subscription_model = api.model('Subscription', {
     'begin_date': fields.Date(required=True, description='Date of the subscription'),
     'end_date': fields.Date(required=True, description='End of the subscription'),
@@ -26,18 +26,15 @@ class SubscriptionList(Resource):
         """Register a new subscription"""
         sub_data = api.payload
         try:
-
             current_user = get_jwt_identity()
             sub_data['user_id'] = current_user['id']
 
-            new_subscription = facade.create_subscription(
-                sub_data
-            )
+            new_subscription = facade.create_subscription(sub_data)
 
             return {
                 'id': new_subscription.id,
-                'begin_date': new_subscription.begin_date,
-                'end_date': new_subscription.end_date,
+                'begin_date': new_subscription.begin_date.isoformat(),
+                'end_date': new_subscription.end_date.isoformat(),
                 'option_nutrition': new_subscription.option_nutrition,
                 'option_message': new_subscription.option_message,
                 'user_id': new_subscription.user_id,
@@ -46,9 +43,9 @@ class SubscriptionList(Resource):
         except ValueError as e:
             return {'error': str(e)}, 400
 
-    @api.response(200, 'List of subscription retrieved successfully')
+    @api.response(200, 'List of subscriptions retrieved successfully')
     def get(self):
-        """Retrieve a list of all subscription"""
+        """Retrieve a list of all subscriptions"""
         subscriptions = facade.get_all_subscriptions()
         return [
             {
@@ -64,50 +61,50 @@ class SubscriptionList(Resource):
         ], 200
 
 
-@api.route('/<message_id>')
-class MessageResource(Resource):
-    @api.response(200, 'message details retrieved successfully')
-    @api.response(404, 'message not found')
-    def get(self, message_id):
-        """Get message with ID"""
+@api.route('/<subscription_id>')
+class SubscriptionResource(Resource):
+    @api.response(200, 'Subscription details retrieved successfully')
+    @api.response(404, 'Subscription not found')
+    def get(self, subscription_id):
+        """Get subscription details by ID"""
         try:
-            message = facade.get_message(message_id)
-            if not message:
-                return {'error': 'message not found'}, 404
+            subscription = facade.get_subscription(subscription_id)
+            if not subscription:
+                return {'error': 'Subscription not found'}, 404
 
             return {
-                'id': message.id,
-                'begin_date': message.begin_date,
-                'end_date': message.end_date,
-                'option_nutrition': message.option_nutrition,
-                'option_message': message.option_message,
-                'user_id': message.user_id,
-                'coach_id': message.coach_id
+                'id': subscription.id,
+                'begin_date': subscription.begin_date.isoformat(),
+                'end_date': subscription.end_date.isoformat(),
+                'option_nutrition': subscription.option_nutrition,
+                'option_message': subscription.option_message,
+                'user_id': subscription.user_id,
+                'coach_id': subscription.coach_id
             }, 200
         except ValueError as e:
             return {'error': str(e)}, 400
 
     @api.expect(subscription_model)
-    @api.response(200, 'subscription updated successfully')
-    @api.response(404, 'subscription not found')
+    @api.response(200, 'Subscription updated successfully')
+    @api.response(404, 'Subscription not found')
     @api.response(400, 'Invalid input data')
     @api.doc(security="token")
     @jwt_required()
     def put(self, subscription_id):
-        """Update a message's information"""
+        """Update a subscription's information"""
         subscription_data = api.payload
         try:
             subscription = facade.get_subscription(subscription_id)
             if not subscription:
-                return {'error': 'message not found'}, 404
+                return {'error': 'Subscription not found'}, 404
 
             current_user = get_jwt_identity()
 
             updated_subscription = facade.update_subscription(subscription_id, subscription_data)
             return {
                 'id': updated_subscription.id,
-                'begin_date': updated_subscription.begin_date,
-                'end_date': updated_subscription.end_date,
+                'begin_date': updated_subscription.begin_date.isoformat(),
+                'end_date': updated_subscription.end_date.isoformat(),
                 'option_nutrition': updated_subscription.option_nutrition,
                 'option_message': updated_subscription.option_message,
                 'user_id': updated_subscription.user_id,
@@ -116,25 +113,27 @@ class MessageResource(Resource):
         except ValueError as e:
             return {'error': str(e)}, 400
 
-    @api.response(200, 'subscription deleted successfully')
-    @api.response(404, 'subscription not found')
+    @api.response(200, 'Subscription deleted successfully')
+    @api.response(404, 'Subscription not found')
+    @api.response(403, 'Unauthorized action')
     @api.doc(security="token")
     @jwt_required()
     def delete(self, subscription_id):
         """Delete a subscription"""
         try:
-            subscription_get = facade.get_subscription(subscription_id)
-            if not subscription_get:
-                return {'error': 'subscription not found'}, 404
+            subscription = facade.get_subscription(subscription_id)
+            if not subscription:
+                return {'error': 'Subscription not found'}, 404
 
             current_user = get_jwt_identity()
 
-            if current_user['id'] == subscription_get.user_id:
+            # Vérifier que l'utilisateur est bien le propriétaire de la subscription
+            if current_user['id'] != subscription.user_id:
                 return {'error': 'Unauthorized action'}, 403
 
-            subscription = facade.delete_subscription(subscription_id)
-            if subscription:
-                return {'message': 'subscription deleted.'}, 200
-            return {'error': 'subscription not found'}, 404
+            result = facade.delete_subscription(subscription_id)
+            if result:
+                return {'message': 'Subscription deleted successfully'}, 200
+            return {'error': 'Subscription not found'}, 404
         except ValueError as e:
             return {'error': str(e)}, 400
